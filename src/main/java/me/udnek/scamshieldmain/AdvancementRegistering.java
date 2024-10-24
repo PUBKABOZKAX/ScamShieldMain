7f09e8d1941e0f497904822aa57fbde8c753aeec
PUBKABOZKAX <g_churakov@list.ru>
2024-10-24 03:27:56 +0300

    advancements

--- src/main/java/me/udnek/scamshieldmain/AdvancementRegistering.java
+++ src/main/java/me/udnek/scamshieldmain/AdvancementRegistering.java
@@ -4,61 +4,128 @@ package me.udnek.scamshieldmain;
 import me.udnek.itemscoreu.customadvancement.AdvancementCriterion;
 import me.udnek.itemscoreu.customadvancement.ConstructableCustomAdvancement;
 import me.udnek.itemscoreu.customadvancement.CustomAdvancementDisplayBuilder;
-import me.udnek.itemscoreu.customadvancement.CustomAdvancementUtils;
-import me.udnek.rpgu.RpgU;
-import me.udnek.rpgu.item.Items;
+import me.udnek.itemscoreu.customitem.CustomItem;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import static me.udnek.itemscoreu.customadvancement.ConstructableCustomAdvancement.RequirementsStrategy.AND;
+import static me.udnek.itemscoreu.customadvancement.ConstructableCustomAdvancement.RequirementsStrategy.OR;
+import static me.udnek.rpgu.item.Items.*;
+import static org.bukkit.Material.*;
 
 
 public class AdvancementRegistering {
     private static final List<ConstructableCustomAdvancement> advancements = new ArrayList<>();
 
     public static void run() {
-        ConstructableCustomAdvancement root = CustomAdvancementUtils.itemAdvancement(new NamespacedKey(ScamShieldMain.getInstance(), "root"), Items.FABRIC.getItem());
-        root.removeCriterion("0");
+
+        ConstructableCustomAdvancement root = generator("root", FABRIC.getItem(), null);
+        Objects.requireNonNull(root.getDisplay()).background("textures/block/cobblestone.png");
         root.addCriterion("tick", AdvancementCriterion.TICK);
-        CustomAdvancementDisplayBuilder displayRoot = new CustomAdvancementDisplayBuilder(Items.FABRIC.getItem());
-        displayRoot.title(Component.translatable("advancement.scamshieldmain.root.title"));//Приветствуем на сервере ScumShield.
-        displayRoot.description(Component.translatable("advancement.scamshieldmain.root.description"));//Мы подготовили для вас цепочку квестов чтобы вы разобрались со всеми изменениями, идите по квестам  и вы увидите все нововидения.
-        root.display(displayRoot);
-        root.getDisplay().background("textures/block/cobblestone.png");
+        root.getDisplay().announceToChat(true);
         advancements.add(root);
 
-
-        ConstructableCustomAdvancement woodenTools = CustomAdvancementUtils.itemAdvancement(new NamespacedKey(ScamShieldMain.getInstance(), "first_tools"), new ItemStack(Material.WOODEN_PICKAXE));
-        woodenTools.removeCriterion("0");
-        CustomAdvancementDisplayBuilder displayWoodenTools = new CustomAdvancementDisplayBuilder(new ItemStack(Material.WOODEN_PICKAXE));
-        displayWoodenTools.title(Component.translatable("advancement.scamshieldmain.first_tools.title"));//Первые инструметы
-        displayWoodenTools.description(Component.translatable("advancement.scamshieldmainu.first_tools.description"));//Отправляйтесь в свое путешествие, приобретя первые простые инструменты.
-        displayWoodenTools.announceToChat(false);
-        woodenTools.display(displayWoodenTools);
-        woodenTools.requirementsStrategy(ConstructableCustomAdvancement.RequirementsStrategy.OR);
-        woodenTools.addCriterion("wooden_pickaxe", AdvancementCriterion.INVENTORY_CHANGE.create(new ItemStack(Material.WOODEN_PICKAXE)));
-        woodenTools.addCriterion("wooden_axe", AdvancementCriterion.INVENTORY_CHANGE.create(new ItemStack(Material.WOODEN_AXE)));
-        woodenTools.addCriterion("wooden_shovel", AdvancementCriterion.INVENTORY_CHANGE.create(new ItemStack(Material.WOODEN_SHOVEL)));
-        woodenTools.addCriterion("wooden_hoe", AdvancementCriterion.INVENTORY_CHANGE.create(new ItemStack(Material.WOODEN_HOE)));
-        woodenTools.addCriterion("wooden_sword", AdvancementCriterion.INVENTORY_CHANGE.create(new ItemStack(Material.WOODEN_SWORD)));
-        woodenTools.parent(root);
+        ConstructableCustomAdvancement woodenTools = generator("first_tools", new ItemStack(Material.WOODEN_PICKAXE), root);
+        addCriteria(woodenTools, OR, WOODEN_PICKAXE, WOODEN_AXE, WOODEN_SHOVEL, WOODEN_HOE, WOODEN_SWORD);
         advancements.add(woodenTools);
 
-        ConstructableCustomAdvancement sand = CustomAdvancementUtils.itemAdvancement(new NamespacedKey(ScamShieldMain.getInstance(), "tough_as_nails"), new ItemStack(Material.SAND));
-        sand.removeCriterion("0");
-        CustomAdvancementDisplayBuilder displaySand = new CustomAdvancementDisplayBuilder(new ItemStack(Material.SAND));
-        displaySand.title(Component.translatable("advancement.scamshieldmain.tough_as_nails.title"));//Попытки побороть жажду.
-        displaySand.description(Component.translatable(""));//
-        sand.display(displaySand);
-        sand.addCriterion("sand", AdvancementCriterion.INVENTORY_CHANGE.create(new ItemStack(Material.SAND)));
-        sand.parent(woodenTools);
-        advancements.add(sand);
+        ConstructableCustomAdvancement fabric = generator("fabric", FABRIC.getItem(), woodenTools);
+        addCriteria(fabric, AND, FABRIC);
+        advancements.add(fabric);
+
+        ConstructableCustomAdvancement leatherArmor = generator("leather_armor", new ItemStack(LEATHER_CHESTPLATE), fabric);
+        addCriteria(leatherArmor, OR, LEATHER_HELMET, LEATHER_CHESTPLATE, LEATHER_LEGGINGS, LEATHER_BOOTS);
+        advancements.add(leatherArmor);
+
+        ConstructableCustomAdvancement ironArmor = generator("iron_armor", new ItemStack(IRON_CHESTPLATE), leatherArmor);
+        addCriteria(ironArmor, OR, IRON_HELMET, IRON_CHESTPLATE, IRON_LEGGINGS, IRON_BOOTS);
+        advancements.add(ironArmor);
+
+        ConstructableCustomAdvancement flint = generator("flint", new ItemStack(FLINT), woodenTools);
+        addCriteria(flint, AND, FLINT);
+        advancements.add(flint);
+
+        ConstructableCustomAdvancement flintTools = generator("flint_tools", FLINT_PICKAXE.getItem(), flint);
+        addCriteria(flintTools, OR, FLINT_PICKAXE, FLINT_AXE, FLINT_SHOVEL, FLINT_HOE, FLINT_SWORD);
+        advancements.add(flintTools);
+
+        ConstructableCustomAdvancement ironTools = generator("iron_tools", new ItemStack(IRON_PICKAXE), flintTools);
+        addCriteria(ironTools, OR, IRON_PICKAXE, IRON_AXE, IRON_SHOVEL, IRON_HOE, IRON_SWORD);
+        advancements.add(ironTools);
+
+        ConstructableCustomAdvancement blastFurnace = generator("blast_furnace", new ItemStack(BLAST_FURNACE), ironTools);
+        addCriteria(blastFurnace, AND, BLAST_FURNACE);
+        advancements.add(blastFurnace);
+
+        ConstructableCustomAdvancement ferrudamEquipment = generator("ferrudam_equipment", FERRUDAM_PICKAXE.getItem(), blastFurnace);
+        addCriteria(ferrudamEquipment, OR, FERRUDAM_PICKAXE, FERRUDAM_AXE, FERRUDAM_SHOVEL, FERRUDAM_HOE, FERRUDAM_SWORD, FERRUDAM_HELMET, FERRUDAM_CHESTPLATE, FERRUDAM_LEGGINGS, FERRUDAM_BOOTS);
+        advancements.add(ferrudamEquipment);
+
+        ConstructableCustomAdvancement netheriteEquipment = generator("netherite_equipment", new ItemStack(NETHERITE_PICKAXE), ferrudamEquipment);
+        addCriteria(netheriteEquipment, OR, NETHERITE_PICKAXE, NETHERITE_AXE, NETHERITE_SHOVEL, NETHERITE_HOE, NETHERITE_SWORD, NETHERITE_HELMET, NETHERITE_CHESTPLATE, NETHERITE_LEGGINGS, NETHERITE_BOOTS);
+        advancements.add(netheriteEquipment);
+
+        ////////////////////////////////////////////////////////////
+
+        ConstructableCustomAdvancement glass = generator("tough_as_nails_root", new ItemStack(GLASS), root);
+        addCriteria(glass, AND, GLASS);
+        advancements.add(glass);
+
+        ConstructableCustomAdvancement bottle = generator("bottle", new ItemStack(GLASS_BOTTLE), glass);
+        addCriteria(bottle, AND, GLASS_BOTTLE);
+        advancements.add(bottle);
+
+        //////////////////////////////////////////////////////////
+
+        ConstructableCustomAdvancement totemSaving = generator("totem_saving", new ItemStack(TOTEM_OF_UNDYING), root);
+        addCriteria(totemSaving, AND, TOTEM_OF_UNDYING);
+        advancements.add(totemSaving);
+
+        ConstructableCustomAdvancement fishermanSnorkel = generator("fisherman_snorkel", new ItemStack(GLASS_BOTTLE), totemSaving);
+        addCriteria(fishermanSnorkel, AND, GLASS);
+        advancements.add(fishermanSnorkel);
+
+        ConstructableCustomAdvancement flowerWreath = generator("flower_wreath", new ItemStack(GLASS_BOTTLE), totemSaving);
+        addCriteria(flowerWreath, AND, GLASS);
+        advancements.add(flowerWreath);
 
         register();
     }
+
+    private static @NotNull ConstructableCustomAdvancement generator(@NotNull String key, @NotNull ItemStack itemStack, @Nullable ConstructableCustomAdvancement parent){
+        ConstructableCustomAdvancement advancement = new ConstructableCustomAdvancement(new NamespacedKey(ScamShieldMain.getInstance(), key));
+        CustomAdvancementDisplayBuilder displayAdvancement = new CustomAdvancementDisplayBuilder(itemStack);
+        displayAdvancement.title(Component.translatable("advancement.scamshieldmain."+ key +".title"));
+        displayAdvancement.description(Component.translatable("advancement.scamshieldmain." + key + ".description"));
+        displayAdvancement.announceToChat(false);
+        advancement.display(displayAdvancement);
+        advancement.parent(parent);
+
+        return advancement;
+    }
+
+    private  static void addCriteria(@NotNull ConstructableCustomAdvancement advancement, @NotNull ConstructableCustomAdvancement.RequirementsStrategy strategy, @NotNull Material ...materials){
+        for (Material material:materials){
+            advancement.addCriterion(material.toString().toLowerCase(), AdvancementCriterion.INVENTORY_CHANGE.create(new ItemStack(material)));
+        }
+        advancement.requirementsStrategy(strategy);
+    }
+
+    private  static void addCriteria(@NotNull ConstructableCustomAdvancement advancement, @NotNull ConstructableCustomAdvancement.RequirementsStrategy strategy, @NotNull CustomItem...customItems){
+        for (CustomItem customItem:customItems){
+            advancement.addCriterion(customItem.getId(), AdvancementCriterion.INVENTORY_CHANGE.create(customItem.getItem()));
+        }
+        advancement.requirementsStrategy(strategy);
+    }
+
     private static void register() {
         for (ConstructableCustomAdvancement advancement : advancements) {
             advancement.register();
