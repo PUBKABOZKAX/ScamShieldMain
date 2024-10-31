24a373e213705bab8e7dd06a814b6ed4bd55bb0b
PUBKABOZKAX <g_churakov@list.ru>
2024-11-01 01:04:43 +0300

    delete Seed

---
+++ src/main/java/me/udnek/scamshieldmain/EventListener.java
@@ -0,0 +1,27 @@
+package me.udnek.scamshieldmain;
+
+import me.udnek.itemscoreu.customevent.InitializationEvent;
+import me.udnek.itemscoreu.util.InitializationProcess;
+import me.udnek.itemscoreu.util.SelfRegisteringListener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EventListener extends SelfRegisteringListener {
+    public EventListener(JavaPlugin plugin) {
+        super(plugin);
+    }
+
+    @EventHandler
+    public void onInit(InitializationEvent event) {
+        if (event.getStep() == InitializationProcess.Step.END) AdvancementRegistering.run();
+    }
+
+    @EventHandler
+    public void deleteSeed(PlayerCommandPreprocessEvent event){
+        if (event.getMessage().equals("/seed")) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage("Ой ты чё ахуел? Команда работает в консоли)))", "Если не доступа то зачем пишиешь это?");
+        }
+    }
+}
