ea2e39ff91e9e3039f22951eb3013e60afd9a1b0
Udnek <gleb.dudorov@gmail.com>
2024-12-11 21:51:43 +0300

    2.0.4

--- src/main/java/me/udnek/scamshieldmain/EventListener.java
+++ src/main/java/me/udnek/scamshieldmain/EventListener.java
@@ -1,17 +1,32 @@
 package me.udnek.scamshieldmain;
 
 import me.udnek.itemscoreu.customevent.InitializationEvent;
+import me.udnek.itemscoreu.customrecipe.RecipeManager;
 import me.udnek.itemscoreu.util.InitializationProcess;
 import me.udnek.itemscoreu.util.SelfRegisteringListener;
+import org.bukkit.Bukkit;
+import org.bukkit.Keyed;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EventListener extends SelfRegisteringListener {
     public EventListener(JavaPlugin plugin) {
         super(plugin);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        Bukkit.recipeIterator().forEachRemaining(recipe ->{
+            if (recipe instanceof Keyed keyed){
+                event.getPlayer().discoverRecipe(keyed.getKey());
+            }
+        });
     }
 
     @EventHandler
